d4613d5a3a16e9972282c2c99c300dbeba46033c
alex <a.gufleer@live.de>
2023-01-30 16:11:31 +0100

    implement core functionality

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,5 +1,77 @@
 class TrackingsController < ApplicationController
-  before_action :set_tracking, only: %i[ show update destroy ]
+  def track
+    @data = {
+      "url" => request.query_string,
+      "ip" => request.remote_ip,
+      "location" => get_location(request.remote_ip),
+      "referrer" => request.referrer,  
+      "operating_system" => get_operating_system(request.env['HTTP_USER_AGENT']),
+      "device" => get_device_type(request.env['HTTP_USER_AGENT']),
+      "language" => request.env['HTTP_ACCEPT_LANGUAGE'],
+      # "test" => request.env['HTTP_USER_AGENT']
+    }
+    # render json: @data
+
+    Tracking.create(
+      url: @data["url"],
+      ip: @data["ip"],
+      location: @data["location"],
+      referrer: @data["referrer"],
+      operating_system: @data["operating_system"],
+      device: @data["device"],
+      language: @data["language"]
+    )
+    render json: Tracking.all
+
+    redirect_to request.query_string, allow_other_host: true
+  end
+
+  private
+    def get_location(ip)
+      require "httparty"
+
+      url = "http://ip-api.com/json/" + ip
+
+      location = HTTParty.get(url)
+
+      if (location["status"]) == "success"
+        return location["country"]
+      else 
+        return location["status"]
+      end
+    end
+
+    def get_operating_system(user_agent)
+      user_agent = user_agent.downcase
+
+      if (user_agent.match(/windows/))
+        return "Windows"
+      elsif (user_agent.match(/linux/))
+        return "Linux"
+      elsif (user_agent.match(/mac/))
+        return "macOS"
+      elsif (user_agent.match(/iphone|ipad/))
+        return "Apple iOS"
+      elsif (user_agent.match(/android/))
+        return "Android"
+      else
+        return "unknown"
+      end
+    end
+
+    def get_device_type(user_agent)
+      user_agent = user_agent.downcase
+      if (user_agent.match(/windows|linux|mac/))
+        return "desktop"
+      elsif (user_agent.match(/iphone|ipad|android/))
+        return "mobile"
+      else
+        return "unknown"
+      end
+    end
+
+=begin
+  # before_action :set_tracking, only: %i[ show update destroy ]
 
   # GET /trackings
   def index
@@ -48,4 +120,5 @@ class TrackingsController < ApplicationController
     def tracking_params
       params.require(:tracking).permit(:url, :ip, :location, :referrer, :operating_system, :device, :language)
     end
+=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :trackings
+  # resources :trackings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/track", to: "trackings#track"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
